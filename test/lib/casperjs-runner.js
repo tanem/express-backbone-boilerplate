ae8d5abc37e5e918307f2bd95219486a64298c1f
Tane Morgan <tane.morgan@gmail.com>
2012-06-22 13:51:17 +1200

    Updated casperjs testrunner for new Jasmine version.

--- test/lib/casperjs-runner.js
+++ test/lib/casperjs-runner.js
@@ -1,9 +1,9 @@
 var casper = require('casper').create();
 
-casper.start('file:///Users/p644205/Sites/_GITHUB_/cube/test/index.html');
+casper.start('http://localhost:8000/test/');
 
-casper.waitForSelector('.finished-at', function () {
-    this.test.assertExists('.runner.passed', this.fetchText('.runner .description'));
+casper.waitForSelector('.alert .bar', function () {
+    this.test.assertExists('.alert .passingAlert', this.fetchText('.alert .bar:nth-child(1)'));
 });
 
 casper.run();
