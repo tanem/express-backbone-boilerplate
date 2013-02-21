0f2f6ac0e3057e5d2b4a456d9030fca76987acc7
Tane Morgan <tanemorgan@gmail.com>
2013-02-21 16:33:40 +1300

    All the things. Installed required grunt tasks, created a few custom ones, updated the Gruntfile to work how we want.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,18 +3,29 @@ module.exports = function(grunt){
   grunt.initConfig({
     jshint: {
       options: {
-        browser: true,
-        es5: true
+        jshintrc: '.jshintrc'
       },
-      src: ['client/src/js/**/*.js', '!client/src/js/lib/*.js'],
-      test: ['client/test/**/*.js', '!client/test/lib/jasmine-1.2.0/*', '!client/test/lib/jasmine-reporters/*.js'],
-      server: ['server/**/*.js', '!server/node_modules/**/*']
+      client: {
+        src: ['client/src/js/**/*.js', '!client/src/js/lib/*.js'],
+        test: ['client/test/**/*.js', '!client/test/lib/jasmine-1.2.0/*', '!client/test/lib/jasmine-reporters/*.js']
+      },
+      server: {
+        src: ['server/**/*.js', '!server/node_modules/**/*'],
+        test: 'server/test/**/*_spec.js'
+      }
+    },
+    clean: {
+      docs: 'docs',
+      junitxml: {
+        client: '_junitxml/cient',
+        server: '_junitxml/server'
+      }
     },
     compass: {
       dev: {
         options: {              
           sassDir: 'client/src/sass',
-          cssDir: 'client/src/css/dev',
+          cssDir: 'client/src/css',
           outputStyle: 'expanded',
           noLineComments: false
         }
@@ -22,7 +33,7 @@ module.exports = function(grunt){
       prod: {
         options: {              
           sassDir: 'client/src/sass',
-          cssDir: 'client/src/css/prod',
+          cssDir: '_dist/css',
           outputStyle: 'compressed',
           noLineComments: true,
           force: true
@@ -35,12 +46,12 @@ module.exports = function(grunt){
         tasks: 'compass:dev'
       },
       jsClient: {
-        files: ['<%= jshint.src %>', '<%= jshint.test %>'],
-        tasks: ['jshint:src', 'jshint:test'/*, 'casperjs'*/]
+        files: ['<%= jshint.client.src %>', '<%= jshint.client.test %>'],
+        tasks: ['jshint:client', 'test-client']
       },
       jsServer: {
-        files: '<%= jshint.server %>',
-        tasks: ['jshint:server', 'jasmine_node']
+        files: ['<%= jshint.server.src %>', '<%= jshint.server.test %>'],
+        tasks: ['jshint:server', 'test-server']
       }
     },
     jasmine_node: {
@@ -52,18 +63,76 @@ module.exports = function(grunt){
       }
     },
     casperjs: {
-      src: 'client/test/lib/casperjs-runner.js'
+      client: {
+        src: 'client/test/lib/casperjs-runner.js'
+      }
+    },
+    docco: {
+      client_src: {
+        options: {
+          output: 'docs/client-src'
+        },
+        src: '<%= jshint.client.src %>'
+      },
+      client_test: {
+        options: {
+          output: 'docs/client-test'
+        },
+        src: '<%= jshint.client.test %>'
+      },
+      server_src: {
+        options: {
+          output: 'docs/server-src'
+        },
+        src: '<%= jshint.server.src %>'
+      },
+      server_test: {
+        options: {
+          output: 'docs/server-test'
+        },
+        src: '<%= jshint.server.test %>'
+      }
+    },
+    docco_index: {
+      client_src: {
+        serve: '/docs/client-src/',
+        src: '<%= docco.client_src.options.output %>',
+        title: 'Client Source',
+        tmpl: 'tasks/assets/index.tmpl'
+      },
+      client_test: {
+        serve: '/docs/client-test/',
+        src: '<%= docco.client_test.options.output %>',
+        title: 'Client Test',
+        tmpl: 'tasks/assets/index.tmpl'
+      },
+      server_src: {
+        serve: '/docs/server-src/',
+        src: '<%= docco.server_src.options.output %>',
+        title: 'Server Source',
+        tmpl: 'tasks/assets/index.tmpl'
+      },
+      server_test: {
+        serve: '/docs/server-test/',
+        src: '<%= docco.server_test.options.output %>',
+        title: 'Server Test',
+        tmpl: 'tasks/assets/index.tmpl'
+      }
     }
   });
 
-  grunt.loadNpmTasks('grunt-casperjs');
+  grunt.loadTasks('tasks');
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-docco');
   grunt.loadNpmTasks('grunt-jasmine-node');
-  
-  grunt.registerTask('server', function(){ require('./server/server.js').listen(3000); });
-  grunt.registerTask('test-client', ['server', 'casperjs']);
+
+  grunt.registerTask('docs', ['clean:docs', 'docco', 'docco_index']);
+  grunt.registerTask('test-client', ['clean:junitxml:client', 'server', 'casperjs']);
+  grunt.registerTask('test-server', ['clean:junitxml:server', 'prep_junitxmldir', 'jasmine_node']);
   grunt.registerTask('start', ['server', 'watch']);
 
 };
--- server/routes/docs.js
+++ server/routes/docs.js
@@ -1,18 +1,18 @@
 var path = require('path'),
-    docsPath = path.join(__dirname, '/../../client/docs');
+  docsPath = path.join(__dirname, '/../../docs');
 
-module.exports = function (app) {
+module.exports = function(app){
+  
+  app.get('/docs/*', function(req, res){      
+    
+    var param = req.params[0];
+    
+    if (/\.(?:html|css)$/.test(param)) {
+      res.sendfile(path.join(docsPath, param));
+    } else {
+      res.sendfile(path.join(docsPath, param, 'index.html'));
+    }
 
-    app.get('/client/docs*', function (req, res) {
-        
-        var param = req.params[0];
-        
-        if (param !== '') {
-            res.sendfile(path.join(docsPath, param));
-        } else {
-            res.sendfile(path.join(docsPath, 'index.html'));
-        }
-
-    });
+  });
 
 };
