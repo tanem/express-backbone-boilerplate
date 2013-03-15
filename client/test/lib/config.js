eadb56a891fbd65f99a35e7d76086c027f774dbb
tanem <tane.morgan@gmail.com>
2013-03-15 19:41:27 +1300

    Syntax update.

--- client/test/lib/config.js
+++ client/test/lib/config.js
@@ -1,33 +1,22 @@
 require.config({
-  
   baseUrl: '/client/src/js',      
-      
   paths: {
-
     jasmine: '/client/test/lib/jasmine-1.3.1/jasmine',
     jasmineHtml: '/client/test/lib/jasmine-1.3.1/jasmine-html',
     jasmineJunitXml: '/client/test/lib/jasmine-reporters/jasmine.junit_reporter',
-    
     tests: '/client/test/lib/tests'
-
   },
-
   shim: {
-
     'jasmine': {
       exports: 'jasmine'
     },
-
     'jasmineHtml': {
       deps: ['jasmine'],
       exports: 'jasmine.HtmlReporter'
     },
-
     'jasmineJunitXml': {
       deps: ['jasmine'],
       exports: 'jasmine.JUnitXmlReporter'
     }
-
   }
-
 });
