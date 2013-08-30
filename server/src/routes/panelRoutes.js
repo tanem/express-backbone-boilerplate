de1e696446c111dcc57bf17905c8f7a164029c13
tanem <tane.morgan@gmail.com>
2013-08-30 13:06:52 +1200

    Decrufting.

--- server/src/routes/panelRoutes.js
+++ server/src/routes/panelRoutes.js
@@ -13,27 +13,3 @@ module.exports = {
   }
 
 };
-/*
-var Panel = require('../models/panel');
-
-module.exports = function(app){
-
-  app.post('/api/panels', function(req, res){
-    var panel = new Panel();
-    panel.save(function(err){
-      if (err) throw err;
-      res.send(panel);
-    });
-  });
-
-  app.delete('/api/panels/:id', function(req, res){
-    var id = req.params.id;
-    Panel.destroy(id, function(err){
-      if (err) throw err;
-      console.log('Panel [%s] deleted.', id);
-      res.send('');
-    });
-
-  });
-
-};*/
