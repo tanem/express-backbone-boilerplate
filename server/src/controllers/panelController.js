28cd208f5b303b9ee29807369b2ddcb1838378ca
tanem <tane.morgan@gmail.com>
2013-09-03 16:47:52 +1200

    Update panelController.js
    Returning properly if an error needs to be sent.

--- server/src/controllers/panelController.js
+++ server/src/controllers/panelController.js
@@ -7,14 +7,14 @@ module.exports = {
 
   create: function(req, res){
     panelModel.create(req.body, function(err, panel){
-      if (err) res.json(err.statusCode, { message: err.message });
+      if (err) return res.json(err.statusCode, { message: err.message });
       res.json(200, panel);
     });
   },
 
   destroy: function(req, res){
     panelModel.destroy(req.params.id, function(err){
-      if (err) res.json(err.statusCode, { message: err.message });
+      if (err) return res.json(err.statusCode, { message: err.message });
       res.json(204, {});
     });
   }
